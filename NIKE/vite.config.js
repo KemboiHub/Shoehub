d35335a22ca60d91c916f32b162e840b77485d65
kemboi <kemboicosmas4160@gmail.com>
2025-06-17 10:32:26 +0300

    added new files

---
+++ NIKE/vite.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // for React or other frameworks
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
