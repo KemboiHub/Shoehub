16b282189b596d2165b1e86981c34a6f38c25d30
kemboi <kemboicosmas4160@gmail.com>
2025-06-19 20:44:01 +0300

    added files

--- NIKE/src/App.jsx
+++ NIKE/src/App.jsx
@@ -1,22 +1,31 @@
-const App = () => (
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './sections';
 
-  <main className = "relative">
-    Nav
-    <section className="xl: padding-1
-    wide: padding-r padding-b">
-      Hero
+const App = () => (
+  <main className="relative">
+    {/* <Nav /> */}
+    <section className="xl:padding-1 wide:pr-8 pb-8">  {/* Fixed Tailwind classes */}
+      <Hero />
     </section>
-    <section className="padding">
-      popularproducts
+    <section className="p-4">  {/* Simplified padding */}
+      <PopularProducts />
     </section>
-    <section className="padding">
-      superQuality
+    <section className="p-4">
+      <SuperQuality />
     </section>
-    <section className="padding-x py-10">
-      services
+    <section className="px-4 py-10">  {/* Tailwind format */}
+      <Services />
     </section>
-    <section className="padding">
-      Specialoffer
+    <section className="p-4">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue p-4">
+      <CustomerReviews />
+    </section>
+    <section className="px-4 sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black px-4 pt-4 pb-8">  {/* Fixed typo */}
+      <Footer />  {/* Render as component */}
     </section>
   </main>
 );
---
+++ NIKE/src/sections/PopularProducts.jsx
@@ -0,0 +1,8 @@
+
+const PopularProduct = () => {
+  return (
+    <div>PopularProducts</div>
+  )
+}
+
+export default PopularProduct
